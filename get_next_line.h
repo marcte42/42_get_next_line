ccd2676ab7f52569a07c5f1561bb044f776c589c
Marc Te <marcte42@gmail.com>
2020-11-23 22:21:29 +0100

    GNL Retry

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 15:03:38 by mterkhoy          #+#    #+#             */
+/*   Updated: 2020/11/23 22:20:40 by mterkhoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+int		ft_process_rest(char **rest, char **line);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mterkhoy <mterkhoy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 21:33:17 by mterkhoy          #+#    #+#             */
+/*   Updated: 2020/11/23 22:20:44 by mterkhoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t			i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t			i;
+
+	i = -1;
+	while (s[++i])
+		if (s[i] == c)
+			return ((char *)&s[i]);
+	return ((c == '\0') ? (char *)&s[i] : NULL);
+}
+
+char	*ft_strncpy(char *dst, const char *src, size_t n)
+{
+	size_t i;
+
+	i = -1;
+	while (src[++i] && i < n)
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dst;
+	size_t	len;
+
+	if (!s1)
+	{
+		if (!(dst = (char *)malloc(sizeof(char))))
+			return (0);
+		dst[0] = '\0';
+	}
+	else
+	{
+		len = ft_strlen(s1);
+		if (!(dst = (char *)malloc((len + 1) * sizeof(char))))
+			return (0);
+		ft_strncpy(dst, s1, len);
+	}
+	return (dst);
+}
+
+char	*ft_strcat(char *s1, const char *s2)
+{
+	ft_strncpy(s1 + ft_strlen(s1), s2, ft_strlen(s2));
+	return (s1);
+}
